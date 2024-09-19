c0f058c4367eb2b404057ae3bd7dcdd97ccfc1d0
sp33dfre4k <sp33dfre4k@proton.me>
2024-09-19 19:44:42 -0400

    Add basic header component with logo

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,11 @@
+import speedfreakLogo from '../../assets/speedfreak.svg';
+
+function Header() {
+    return (
+        <header>
+            <img src={speedfreakLogo} alt="Speedfreak Logo" />
+        </header>
+    );
+}
+
+export default Header;
